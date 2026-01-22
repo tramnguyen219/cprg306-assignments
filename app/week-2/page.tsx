0887a7ebcdcc4ec7c14655928deaf9fedf1e5563
tramnguyen219 <nguyenngoctram.nguyen@edu.sait.ca>
2026-01-22 14:57:11 -0700

    some changes

---
+++ app/week-2/page.tsx
@@ -0,0 +1,11 @@
+import StudentInfor from "./student.info";
+
+
+export default function Page() {
+  return (
+    <main>
+      <h1>Shopping List</h1>
+      <StudentInfor/>
+    </main>
+  );
+}
---
+++ app/week-2/student.info.tsx
@@ -0,0 +1,11 @@
+
+export default function StudentInfo() {
+  return (
+    <div>
+      <h2>Tram Nguyen</h2>
+      <a href="https://github.com/tramnguyen219" target="_blank" rel="noopener noreferrer">
+        https://github.com/tramnguyen219
+      </a>
+    </div>
+  );
+}
