13c14c6fc6264027476c5598029888675f9d72e3
tramnguyen219 <nguyenngoctram.nguyen@edu.sait.ca>
2026-01-29 14:18:00 -0700

    changed for week 3

--- app/page.js
+++ app/page.js
@@ -7,6 +7,12 @@ export default function Home() {
       <Link href="/week-2">
         Week 2
       </Link>
+      <br />
+      <Link href="/week-3">
+           Week 3
+      </Link>
+      
+      
     </main>
   );
 }
